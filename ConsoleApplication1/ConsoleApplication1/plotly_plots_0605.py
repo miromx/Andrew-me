6f871b6f9b56c9ec550ca98e3235f840c0241839
miro <40739539+miromx@users.noreply.github.com>
2019-05-06 17:33:38 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/plotly_plots_0605.py
@@ -0,0 +1,73 @@
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+
+# N = 1000
+# random_x = np.random.randn(N)
+# random_y = np.random.randn(N)
+
+xdata = np.linspace(-10, 10, 101)
+ydata = np.cos(xdata)
+
+trace = go.Scatter(
+    x=xdata,
+    y=ydata,
+    mode='lines+markers',
+    name='cos'
+)
+
+trace1 = go.Scatter(
+    x=xdata,
+    y=np.sin(xdata),
+    mode='lines+markers',
+    name='sin'
+)
+
+data = [trace, trace1]
+
+layout = go.Layout(
+        legend=dict(
+                orientation="h",
+                traceorder='normal',
+                bgcolor='white',
+                bordercolor='black',
+                borderwidth=0.5
+        ),
+        title='Графики',
+        showlegend=True,
+        xaxis=dict(
+            showgrid=True,
+            mirror='ticks',
+            title='Ось-Х',
+            ticks='',
+            gridcolor='grey',
+            gridwidth=0.5,
+            zerolinecolor='black',
+            zerolinewidth=0.25,
+            linecolor='black',
+            linewidth=0.5),
+        yaxis=dict(
+            showgrid=True,
+            mirror='ticks',
+            title='Ось-У',
+            ticks='',
+            gridcolor='grey',
+            gridwidth=0.5,
+            zerolinecolor='black',
+            zerolinewidth=0.25,
+            linecolor='black',
+            linewidth=0.5),
+        width=800,
+        height=600
+
+)
+
+config = {
+    'linkText': "a&m",
+    'scrollZoom': True,
+    'displayModeBar': True,
+    'editable': True
+}
+
+# plotly.offline.plot(data, config=config, filename='basic-scatter.html', auto_open=False)
+plotly.offline.plot(dict(data=data,layout=layout), config=config, filename='simple-plots.html', auto_open=True)
